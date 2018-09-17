da2fea276777ff47ac64bc29a54a86527ce568d8
Yongho Lee <ylee01@stuy.edu>
2018-09-17 11:11:49 -0400

    Added file

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello, my name is Yongho Lee, and I am 16 years old, born on June 21st, 2002.");
+   }
+}
